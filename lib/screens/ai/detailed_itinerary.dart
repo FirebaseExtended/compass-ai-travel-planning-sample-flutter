3e393994c10e6caab55b9fc8fcb326af3d5322d8
Khanh Nguyen <khanhnwin@gmail.com>
2024-06-05 14:03:08 -0700

    edited app bar config

--- lib/screens/ai/detailed_itinerary.dart
+++ lib/screens/ai/detailed_itinerary.dart
@@ -42,16 +42,41 @@ class _DetailedItineraryState extends State<DetailedItinerary> {
                       Text(
                         '14th May - 21st May, 2024',
                         style: TextStyle(
-                          fontSize: 16,
+                          fontSize: 14,
                           color: Theme.of(context).colorScheme.onPrimary,
                         ),
                       ),
                     ],
                   ),
                 ),
-                background: Image.asset(
-                  fit: BoxFit.cover,
-                  'assets/images/paris.png',
+                background: Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        'assets/images/paris.png',
+                      ),
+                    ),
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 300,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: FractionalOffset.bottomCenter,
+                            end: FractionalOffset.topCenter,
+                            colors: [
+                              Color.fromARGB(150, 49, 49, 49),
+                              Colors.transparent
+                            ],
+                            stops: [
+                              0.0,
+                              0.75
+                            ]),
+                      ),
+                    ),
+                  ),
                 ),
               ),
               backgroundColor: Colors.transparent,
@@ -85,67 +110,72 @@ class _DetailedItineraryState extends State<DetailedItinerary> {
             SliverList(
               delegate: SliverChildListDelegate(
                 [
-                  const DayTitle(title: 'Day 1'),
-                  Stepper(
-                    controlsBuilder: (context, details) {
-                      return const SizedBox.shrink();
-                    },
-                    stepIconWidth: 80,
-                    stepIconHeight: 80,
-                    currentStep: activeStep,
-                    stepIconBuilder: (stepIndex, stepState) {
-                      if (stepIndex == activeStep) {
-                        return Container(
-                          width: 80,
-                          height: 80,
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(4),
+                  Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    const DayTitle(title: 'Day 1'),
+                    SizedBox(
+                      child: Stepper(
+                        margin: const EdgeInsets.only(top: 0),
+                        controlsBuilder: (context, details) {
+                          return const SizedBox.shrink();
+                        },
+                        stepIconWidth: 80,
+                        stepIconHeight: 80,
+                        currentStep: activeStep,
+                        stepIconBuilder: (stepIndex, stepState) {
+                          if (stepIndex == activeStep) {
+                            return Container(
+                              width: 80,
+                              height: 80,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(4),
+                                ),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage('assets/images/louvre.png'),
+                                ),
+                                color: Colors.green, // b
+                              ),
+                            );
+                          }
+                          return Container(
+                            width: 28,
+                            height: 28,
+                            decoration: const BoxDecoration(
+                              color: Colors.yellow, // border color
+                              shape: BoxShape.circle,
                             ),
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage('assets/images/louvre.png'),
-                            ),
-                            color: Colors.green, // b
+                          );
+                        },
+                        onStepTapped: (value) {
+                          setState(() {
+                            activeStep = value;
+                          });
+                        },
+                        steps: const [
+                          Step(
+                            title: Text('Louvre Museum Guided Tour'),
+                            subtitle: Text('May 14, Morning'),
+                            content: Text(
+                                "Explore the Louvre's treasures with a guided tour unveils art's rich history."),
                           ),
-                        );
-                      }
-                      return Container(
-                        width: 28,
-                        height: 28,
-                        decoration: const BoxDecoration(
-                          color: Colors.yellow, // border color
-                          shape: BoxShape.circle,
-                        ),
-                      );
-                    },
-                    onStepTapped: (value) {
-                      setState(() {
-                        activeStep = value;
-                      });
-                    },
-                    steps: const [
-                      Step(
-                        title: Text('Louvre Museum Guided Tour'),
-                        subtitle: Text('May 14, Morning'),
-                        content: Text(
-                            "Explore the Louvre's treasures with a guided tour unveils art's rich history."),
+                          Step(
+                            title: Text('Seine River Cruise'),
+                            content: Text(
+                              "Parisian allure awaits on the Seine River aboard traditional, luxurious vessels.",
+                            ),
+                          ),
+                          Step(
+                            title: Text('Louvre Museum Guided Tour'),
+                            content: Text(
+                                "Explore the Louvre's treasures with a guided tour unveils art's rich history."),
+                          ),
+                        ],
                       ),
-                      Step(
-                        title: Text('Seine River Cruise'),
-                        content: Text(
-                          "Parisian allure awaits on the Seine River aboard traditional, luxurious vessels.",
-                        ),
-                      ),
-                      Step(
-                        title: Text('Louvre Museum Guided Tour'),
-                        content: Text(
-                            "Explore the Louvre's treasures with a guided tour unveils art's rich history."),
-                      ),
-                    ],
-                  ),
-                  const DayTitle(title: 'Day 2'),
-                  const DayTitle(title: 'Day 3'),
+                    ),
+                    const DayTitle(title: 'Day 2'),
+                    const DayTitle(title: 'Day 3'),
+                  ])
                 ],
               ),
             )
