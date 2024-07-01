8a7839a6cf63c28f309cecfc6228181973fd264d
Khanh Nguyen <KhanhNwin@gmail.com>
2024-07-01 16:25:19 -0400

    more rough edits for activities screen

--- lib/screens/legacy/activities/activities_screen.dart
+++ lib/screens/legacy/activities/activities_screen.dart
@@ -13,48 +13,109 @@ class _ActivitiesScreenState extends State<ActivitiesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Activities'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: IconButton(
-              style: ButtonStyle(
-                side: WidgetStatePropertyAll(
-                  BorderSide(color: Colors.grey[300]!),
+        appBar: AppBar(
+          title: const Text('Activities'),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: IconButton(
+                style: ButtonStyle(
+                  side: WidgetStatePropertyAll(
+                    BorderSide(color: Colors.grey[300]!),
+                  ),
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
                 ),
-                shape: WidgetStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+                onPressed: () => context.go('/'),
+                icon: const Icon(
+                  Icons.home_outlined,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 4),
+                child: Text(
+                  'Daytime',
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
               ),
-              onPressed: () => context.go('/'),
-              icon: const Icon(
-                Icons.home_outlined,
+              ActivityTile(),
+              ActivityTile(),
+              ActivityTile(),
+              SizedBox.square(
+                dimension: 16,
               ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 4),
+                child: Text(
+                  'Evening',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              ActivityTile(),
+              ActivityTile(),
+              ActivityTile(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(24, 16, 24, 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '3 Selected',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    padding: const WidgetStatePropertyAll(
+                      EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 18,
+                      ),
+                    ),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    backgroundColor: WidgetStatePropertyAll(
+                      Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  child: Text(
+                    'Confirm',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 8,
-        ),
-        child: ListView(
-          children: [
-            Text('Daytime'),
-            ActivityTile(),
-            ActivityTile(),
-            ActivityTile(),
-            Text('Evening'),
-            ActivityTile(),
-            ActivityTile(),
-            ActivityTile(),
-          ],
-        ),
-      ),
-    );
+        ));
   }
 }
