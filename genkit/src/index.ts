24880f9c48956b135d56aa6ce83bc581645fb034
nohe <nohe@google.com>
2024-07-12 17:48:38 +0000

    Remove elementry flags

--- genkit/src/index.ts
+++ genkit/src/index.ts
@@ -1,7 +1,6 @@
 import { configureGenkit } from '@genkit-ai/core';
 import { startFlowsServer } from '@genkit-ai/flow';
 import { vertexAI } from '@genkit-ai/vertexai';
-import { firebase } from '@genkit-ai/firebase';
 import { googleAI } from '@genkit-ai/googleai';
 import { dotprompt } from '@genkit-ai/dotprompt';
 
@@ -9,43 +8,18 @@ import { dotprompt } from '@genkit-ai/dotprompt';
 import * as tools from './tools'
 import * as flows from './flows'
 import * as retrievers from './retrievers';
-import googleCloud from '@genkit-ai/google-cloud';
-import { AlwaysOnSampler } from '@opentelemetry/sdk-trace-base';
 
 configureGenkit({
   plugins: [
-    firebase({
-      flowStateStore: { collection: 'flowTraceStore' },
-    }),
     googleAI(),
     // Uncomment to use Vertex AI instead.
     // vertexAI({
     //   location: 'us-central1',
     // }),
     dotprompt({ dir: 'prompts' }),
-    googleCloud({
-      forceDevExport: true, // Set this to true to export telemetry for local runs
-      telemetryConfig: {
-        sampler: new AlwaysOnSampler(),
-        autoInstrumentation: true,
-        autoInstrumentationConfig: {
-          '@opentelemetry/instrumentation-fs': { enabled: false },
-          '@opentelemetry/instrumentation-dns': { enabled: false },
-          '@opentelemetry/instrumentation-net': { enabled: false },
-        },
-        metricExportIntervalMillis: 5_000,
-        metricExportTimeoutMillis: 5_000,
-      },
-    }),
   ],
-  flowStateStore: 'firebase',
-  traceStore: 'firebase',
   enableTracingAndMetrics: true,
   logLevel: 'debug',
-  telemetry: {
-    instrumentation: 'googleCloud',
-    logger: 'googleCloud',
-  },
 });
 
 // Making our tooling discoverable as endpoints to the flow server.
