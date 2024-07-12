303b8b8ad08214d1f15ae3dafad0e74e7f9c5a3f
Khanh Nguyen <KhanhNwin@gmail.com>
2024-07-12 19:24:54 -0400

    Separate Compass AI Flow theme

--- lib/ai/styles.dart
+++ lib/ai/styles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // Component Styles
 ButtonStyle get appBarSquareButtonStyle => ButtonStyle(
@@ -9,4 +10,13 @@ ButtonStyle get appBarSquareButtonStyle => ButtonStyle(
       ),
       backgroundColor: WidgetStatePropertyAll(Colors.grey[300]),
       iconColor: const WidgetStatePropertyAll(Colors.black87),
+    );
+
+// App Theme
+ThemeData get compassAITheme => ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xff7B4E7F),
+      ),
+      textTheme: GoogleFonts.rubikTextTheme(),
+      useMaterial3: true,
     );
