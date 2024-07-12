e1890a9a38b9151e20739da7e1f0cd6d9015e143
Khanh Nguyen <KhanhNwin@gmail.com>
2024-07-12 19:27:52 -0400

    start shared navigation file

--- lib/common/services/navigation.dart
+++ lib/common/services/navigation.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 void goToSplashScreen(BuildContext context) {
-  () => goToSplashScreen(context);
+  context.go('/');
 }
