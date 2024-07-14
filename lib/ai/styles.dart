3188dfe9c2840f3f71b665ed06591c6d7b6b7fc4
Khanh Nguyen <KhanhNwin@gmail.com>
2024-07-14 12:24:49 -0400

    add breakpoints

--- lib/ai/styles.dart
+++ lib/ai/styles.dart
@@ -22,3 +22,10 @@ ThemeData get compassAITheme => ThemeData(
       textTheme: GoogleFonts.rubikTextTheme(),
       useMaterial3: true,
     );
+
+class Breakpoints {
+  static get compact => 600;
+  static get medium => 840;
+  static get expanded => 1200;
+  static get large => 1600;
+}
