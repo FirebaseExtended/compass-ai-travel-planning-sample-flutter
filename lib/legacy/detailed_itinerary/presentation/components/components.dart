5fc7028a8719dc2f6d0b134a011ba992f9be1358
Khanh Nguyen <KhanhNwin@gmail.com>
2024-07-15 15:12:46 -0400

    refactor legacy detailed itinerary

--- lib/legacy/detailed_itinerary/presentation/components/components.dart
+++ lib/legacy/detailed_itinerary/presentation/components/components.dart
@@ -1,1 +1,2 @@
 export './share_trip.dart';
+export './responsive_layouts.dart';
--- lib/legacy/detailed_itinerary/presentation/legacy_itinerary.dart
+++ lib/legacy/detailed_itinerary/presentation/legacy_itinerary.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tripedia/legacy/activities_feature/presentation/activity_list_tile.dart';
-import 'package:tripedia/legacy/common/widgets/tag_chip.dart';
-import 'package:tripedia/common/utilties.dart';
+import 'package:tripedia/ai/styles.dart';
 
 import '../../activities_feature/models/activity.dart';
-import '../../../common/services/navigation.dart';
 import './components/components.dart';
-import '../../form_feature/models/travel_query.dart';
-import '../../results/business/model/destination.dart';
+import '../../../common/services/navigation.dart';
 
 class LegacyItinerary extends StatefulWidget {
   const LegacyItinerary({super.key});
@@ -20,6 +16,8 @@ class LegacyItinerary extends StatefulWidget {
 class _LegacyItineraryState extends State<LegacyItinerary> {
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.sizeOf(context).width;
+    var displaySmallLayout = width <= Breakpoints.medium;
     var query = context.watch<TravelPlan>().query;
     var destination = context.watch<TravelPlan>().destination;
     var activities = context.watch<TravelPlan>().activities;
@@ -37,199 +35,48 @@ class _LegacyItineraryState extends State<LegacyItinerary> {
         ),
       ),
       child: Scaffold(
-        body: SmallLegacyItinerary(
-          query: query,
-          destination: destination,
-          activities: activities,
-        ),
-        bottomNavigationBar: const SafeArea(
-          child: ShareTripButton(),
-        ),
+        appBar: !displaySmallLayout
+            ? AppBar(
+                actions: [
+                  Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: IconButton(
+                        style: ButtonStyle(
+                          side: WidgetStatePropertyAll(
+                            BorderSide(color: Colors.grey[300]!),
+                          ),
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                        ),
+                        onPressed: () => goToSplashScreen(context),
+                        icon: const Icon(
+                          Icons.home_outlined,
+                        ),
+                      )),
+                  const SizedBox(
+                    width: 8,
+                  )
+                ],
+              )
+            : null,
+        body: displaySmallLayout
+            ? SmallLegacyItinerary(
+                query: query,
+                destination: destination,
+                activities: activities,
+              )
+            : LargeLegacyItinerary(
+                query: query, destination: destination, activities: activities),
+        bottomNavigationBar: displaySmallLayout
+            ? const SafeArea(
+                child: ShareTripButton(),
+              )
+            : null,
       ),
     );
-  }
-}
-
-class SmallLegacyItinerary extends StatelessWidget {
-  const SmallLegacyItinerary(
-      {required this.query,
-      required this.destination,
-      required this.activities,
-      super.key});
-
-  final TravelQuery query;
-  final Destination destination;
-  final Set<LegacyActivity> activities;
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          expandedHeight: 240,
-          centerTitle: true,
-          flexibleSpace: FlexibleSpaceBar(
-            titlePadding: const EdgeInsets.fromLTRB(16, 0, 0, 16),
-            centerTitle: true,
-            expandedTitleScale: 2.25,
-            title: Align(
-              alignment: Alignment.bottomLeft,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    destination.name,
-                    maxLines: 2,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
-                  ),
-                  Text(
-                    '${prettyDate(query.dates.start.toString())} - ${prettyDate(query.dates.end.toString())}',
-                    maxLines: 2,
-                    style: TextStyle(
-                      fontSize: 10,
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            background: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                    destination.imageUrl,
-                  ),
-                ),
-              ),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: 210,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: FractionalOffset.bottomCenter,
-                        end: FractionalOffset.topCenter,
-                        colors: [
-                          Color.fromARGB(165, 0, 0, 0),
-                          Colors.transparent
-                        ],
-                        stops: [
-                          0.2,
-                          1
-                        ]),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          backgroundColor: Colors.transparent,
-          leading: Padding(
-            padding: const EdgeInsets.all(8),
-            child: IconButton(
-              style: ButtonStyle(
-                shape: WidgetStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-                backgroundColor: const WidgetStatePropertyAll(
-                    Color.fromARGB(170, 148, 136, 136)),
-              ),
-              icon: Icon(
-                Icons.arrow_back,
-                color: Theme.of(context).colorScheme.onPrimary,
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ),
-          actions: [
-            Padding(
-                padding: const EdgeInsets.all(8),
-                child: IconButton(
-                  style: ButtonStyle(
-                    shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                    backgroundColor: const WidgetStatePropertyAll(
-                        Color.fromARGB(170, 148, 136, 136)),
-                  ),
-                  onPressed: () => goToSplashScreen(context),
-                  icon: Icon(
-                    Icons.home_outlined,
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
-                )),
-          ],
-        ),
-        SliverPadding(
-          padding: const EdgeInsets.fromLTRB(24, 16, 24, 16),
-          sliver: SliverList(
-            delegate: SliverChildListDelegate([
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 0),
-                child: Text(
-                  destination.knownFor,
-                  style: const TextStyle(
-                    fontSize: 14,
-                  ),
-                ),
-              ),
-              const SizedBox.square(
-                dimension: 16,
-              ),
-              SizedBox(
-                height: 30,
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: destination.tags.length,
-                  itemBuilder: (context, index) =>
-                      DetailTagChip(tag: destination.tags[index]),
-                  separatorBuilder: (context, index) {
-                    return const SizedBox.square(
-                      dimension: 8,
-                    );
-                  },
-                ),
-              ),
-              const SizedBox.square(
-                dimension: 24,
-              ),
-              const Text(
-                'Your Chosen Activities',
-                style: TextStyle(fontSize: 18),
-              ),
-              ...List.generate(
-                activities.length,
-                (activityIndex) {
-                  return ActivityDetailTile(
-                    activity: activities.elementAt(activityIndex),
-                  );
-                },
-              )
-            ]),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class LargeLegacyItinerary extends StatelessWidget {
-  const LargeLegacyItinerary({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
 
