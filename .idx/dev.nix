fc9b27b6dee5959a7954663917e9f2749126e4e1
Rody Davis <rody.davis.jr@gmail.com>
2024-11-11 11:00:26 -0800

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -42,10 +42,6 @@
   idx.workspace = {
     # Runs when a workspace is first created with this `dev.nix` file
     onCreate = {
-      # git-lfs-fetch = ''
-      #   curl -o local.zip 'https://firebasestorage.googleapis.com/v0/b/yt-rag.appspot.com/o/genkit%2Flocal.zip?alt=media&token=2f1d181d-9eda-4dc1-9ffc-e988f69c26f2'
-      #   unzip local.zip -d .
-      # '';
       npm-install = ''
         cd proxy
         npm ci
