b7139cc4766ff734a4025bb5f941a3d55ce3ef3b
Maneesh Tewani <mtewani@google.com>
2024-11-19 11:37:45 -0800

    Fixed dev.nix to use the latest version of the VSCode extension

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,82 +1,76 @@
 {pkgs, ...}:
-  let firebase-ext = pkgs.fetchurl {
-    url =
-      "https://firebasestorage.googleapis.com/v0/b/firemat-preview-drop/o/vsix%2Ffirebase-vscode-idx-0.1.7.vsix?alt=media&token=83902099-e9e4-4ee8-b953-2deb6a3e0167";
-    hash = "sha256-OAYOxRcFyUR2tiaH1IdNmJeOhfR5c8G32bkqvzZZbbc=";
-    name = "firebase.vsix";
-  };
-  in {
-  channel = "stable-24.05";
-  packages = [
-    pkgs.nodePackages.firebase-tools
-    pkgs.jdk17
-    pkgs.unzip
-    (pkgs.postgresql_15.withPackages (p: [ p.pgvector ]))
-    pkgs.nodejs_20
-    pkgs.yarn
-    pkgs.nodePackages.pnpm
-    pkgs.bun
-    pkgs.zip
-    pkgs.curl
-  ];
-  env = {
-    POSTGRESQL_CONN_STRING = "postgresql://user:mypassword@localhost:5432/dataconnect?sslmode=disable";
-    FIRESQL_PORT = "9939";
-    # Sets environment variables in the workspace
-    # You can get a Gemini API key through the IDX Integrations panel to the left!
-    GOOGLE_API_KEY = "REPLACE_ME";
-    MAPS_API_KEY = "";
-  };
-  processes = {
-    postgresRun = {
-      command = "postgres -D ./local -k /tmp";
+  {
+    channel = "stable-24.05";
+    packages = [
+      pkgs.nodePackages.firebase-tools
+      pkgs.jdk17
+      pkgs.unzip
+      (pkgs.postgresql_15.withPackages (p: [ p.pgvector ]))
+      pkgs.nodejs_20
+      pkgs.yarn
+      pkgs.nodePackages.pnpm
+      pkgs.bun
+      pkgs.zip
+      pkgs.curl
+    ];
+    env = {
+      POSTGRESQL_CONN_STRING = "postgresql://user:mypassword@localhost:5432/dataconnect?sslmode=disable";
+      FIRESQL_PORT = "9939";
+      # Sets environment variables in the workspace
+      # You can get a Gemini API key through the IDX Integrations panel to the left!
+      GOOGLE_API_KEY = "REPLACE_ME";
+      MAPS_API_KEY = "";
     };
-  };
-  idx.extensions = [
-    "Dart-Code.flutter"
-    "mtxr.sqltools-driver-pg"
-    "mtxr.sqltools"
-    "GraphQL.vscode-graphql-syntax"
-    "${firebase-ext}"
-  ];
-  idx.workspace = {
-    # Runs when a workspace is first created with this `dev.nix` file
-    onCreate = {
-      npm-install = ''
-        cd proxy
-        npm ci
-      '';
-    };
-    onStart = {
-      genkit-start = ''
-        cd genkit
-        npm install
-        npm install --only=dev
-        npx genkit-cli start -- npx tsx --watch src/index.ts
-      '';
-      flutter-start = ''
-        flutter upgrade
-        flutter run --machine -d web-server --web-hostname 0.0.0.0 --web-port 6789 --dart-define=WEB_HOST=9000-$WEB_HOST
-      '';
-    };
-  };
-  idx.previews = {
-    previews = {
-      web = {
-        command = [
-          "npm"
-          "run"
-          "--prefix" 
-          "proxy"
-          "dev"
-          "--"
-          "--host"
-          "0.0.0.0"
-          "--port"
-          "$PORT"
-        ];
-        manager = "flutter";
+    processes = {
+      postgresRun = {
+        command = "postgres -D ./local -k /tmp";
       };
     };
-  };
-}
+    idx.extensions = [
+      "Dart-Code.flutter"
+      "mtxr.sqltools-driver-pg"
+      "mtxr.sqltools"
+      "GraphQL.vscode-graphql-syntax"
+      "GoogleCloudTools.firebase-dataconnect-vscode"
+    ];
+    idx.workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        npm-install = ''
+          cd proxy
+          npm ci
+        '';
+      };
+      onStart = {
+        genkit-start = ''
+          cd genkit
+          npm install
+          npm install --only=dev
+          npx genkit-cli start -- npx tsx --watch src/index.ts
+        '';
+        flutter-start = ''
+          flutter upgrade
+          flutter run --machine -d web-server --web-hostname 0.0.0.0 --web-port 6789 --dart-define=WEB_HOST=9000-$WEB_HOST
+        '';
+      };
+    };
+    idx.previews = {
+      previews = {
+        web = {
+          command = [
+            "npm"
+            "run"
+            "--prefix" 
+            "proxy"
+            "dev"
+            "--"
+            "--host"
+            "0.0.0.0"
+            "--port"
+            "$PORT"
+          ];
+          manager = "flutter";
+        };
+      };
+    };
+  }
